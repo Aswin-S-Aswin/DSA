571451e2aa90e32e9ca3b3e5bcb23c5af201ea0e
Aswin_S_Aswin <rtraswinppgit@gmail.com>
2025-12-20 23:09:27 +0530

    new

---
+++ test html/Shopping_cart.py
@@ -0,0 +1,20 @@
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (Q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        foods.append(food)
+        price = float(input(f"Enter the price of {food} ($): "))
+        prices.append(price)
+
+print("----- Your cart -----")
+for food in foods:
+    print(food, end=" ")
+print()
+for price in prices:
+    total += price
+print(f"Your total is: ${total}")
