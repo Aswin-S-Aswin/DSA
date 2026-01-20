52c9190c91a3a23f6a51e5d780d98d14b5cb25f1
Aswin_S_Aswin <rtraswinppgit@gmail.com>
2026-01-20 23:13:00 +0530

    New files

--- DSA/src/LinkedList_vs_ArrayList.java
+++ DSA/src/LinkedList_vs_ArrayList.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 
 public class LinkedList_vs_ArrayList {
     public static void main(String[] args) {
+
         LinkedList<Integer> link = new LinkedList<>();
         ArrayList<Integer> arr = new ArrayList<>();
 
