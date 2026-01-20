4886121481220a2fe65d17d5799331f6b8852293
Aswin_S_Aswin <rtraswinppgit@gmail.com>
2026-01-20 23:12:13 +0530

    New files

---
+++ DSA/src/LinkedList_vs_ArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LinkedList_vs_ArrayList {
+    public static void main(String[] args) {
+        LinkedList<Integer> link = new LinkedList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        long startTime;
+        long endTime;
+        long elapsedTime;
+
+    }
+}
